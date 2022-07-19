682bddf7c566505619899b5bae22bf6f61f726c6
Rong Wang <478569657@qq.com>
2022-07-19 20:15:26 +0100

    Plot and compare the specific heat from this work, JANAF and nasa

--- specificheat.py
+++ specificheat.py
@@ -97,8 +97,8 @@ for file in tqdm(files):
             for i in range(len(list_sorted)):
                 T_this.append(list_sorted[i][0])
                 Cp_this.append(list_sorted[i][1])
-            Cp_df=pd.DataFrame({'T(K)': T_this, 'Cp_Thiswork': Cp_this})
-            Cp_df.to_csv(storename,index=False)
+            # Cp_df=pd.DataFrame({'T(K)': T_this, 'Cp_Thiswork': Cp_this})
+            # Cp_df.to_csv(storename,index=False)
 
             # Store the maximum temperature in HITRAN database
             Tmax_this=T_this[-1]
@@ -142,26 +142,26 @@ for file in tqdm(files):
 
 # %%
 # Generate a JANAF dictionary 
-# path = "JANAF"
-# files= os.listdir(path) 
-# Cp_JANAF=dict()
-# for file in tqdm(files): 
-#      if ".txt" in file:
-#         if not os.path.isdir(file): 
-#             filename=path+"/"+file
-#             molecule=file.split(".")[0]
-#             print(molecule)
-#             # Read partition functions from database
-#             T,Cp=np.loadtxt(filename,usecols=(0,1),unpack=True)
+path = "JANAF"
+files= os.listdir(path) 
+Cp_JANAF=dict()
+for file in tqdm(files): 
+     if ".txt" in file:
+        if not os.path.isdir(file): 
+            filename=path+"/"+file
+            molecule=file.split(".")[0]
+            print(molecule)
+            # Read partition functions from database
+            T,Cp=np.loadtxt(filename,usecols=(0,1),unpack=True)
 
-#             for i in range(len(T)):
-#                 t=T[i]    
-#                 cp=float(Cp[i])  
-#                 if molecule in Cp_JANAF:
-#                     Cp_JANAF[molecule][t]=cp
-#                 else:
-#                     Cp_JANAF[molecule]=dict()
-#                     Cp_JANAF[molecule][t]=cp  
+            for i in range(len(T)):
+                t=T[i]    
+                cp=float(Cp[i])  
+                if molecule in Cp_JANAF:
+                    Cp_JANAF[molecule][t]=cp
+                else:
+                    Cp_JANAF[molecule]=dict()
+                    Cp_JANAF[molecule][t]=cp  
 
 
 # filename2='Cp_JANAFdict.json'
@@ -219,8 +219,80 @@ with open('nasa9.dat', 'r') as nasa_file:
                        
         else:
             pass
+
 # %%
-fit_nasa['CO'][0][1]
-# %%
-temprange_nasa['NO2']
+for molecule in tqdm(Cp_dict):
+
+    #print JANAF
+    exist_JANAF=False
+    Tmax=Tmax_dict[molecule]
+    if molecule in Cp_JANAF:
+        exist_JANAF=True
+        J_temp=Cp_JANAF[molecule]
+        T_J=[]
+        Cp_J=[]
+        for t in J_temp:
+            if (float(t) <=Tmax) & (float(t) >=298):
+                T_J.append(float(t))
+                Cp_J.append(float(J_temp[t])) 
+        plt.plot(T_J,Cp_J,color="blue",label="JANAF")  
+
+    exist_nasa=False
+
+    if Tmax<=1000:
+        x=np.arange(298.15, Tmax, 0.01)
+        coefficient_this=fit_dictionary[molecule][0]
+        Cp_fit_this=R*(coefficient_this[0]*x**(-2)+coefficient_this[1]*x**(-1)+coefficient_this[2]+coefficient_this[3]*x+coefficient_this[4]*x**2+coefficient_this[5]*x**3+coefficient_this[6]*x**4)
+        # if molecule in fit_nasa:
+        #     exist_nasa=True
+            # coefficient_nasa=fit_nasa[molecule][0]
+            # Cp_fit_nasa=R*(coefficient_nasa[0]*x**(-2)+coefficient_nasa[1]*x**(-1)+coefficient_nasa[2]+coefficient_nasa[3]*x+coefficient_nasa[4]*x**2+coefficient_nasa[5]*x**3+coefficient_nasa[6]*x**4)
+    else:
+        x1=np.arange(298.15, 1000, 0.01)
+        coefficient_this1=fit_dictionary[molecule][0]
+        Cp_fit_this1=R*(coefficient_this1[0]*x1**(-2)+coefficient_this1[1]*x1**(-1)+coefficient_this1[2]+coefficient_this1[3]*x1+coefficient_this1[4]*x1**2+coefficient_this1[5]*x1**3+coefficient_this1[6]*x1**4)
+        if molecule in fit_nasa:
+            coefficient_nasa1=fit_nasa[molecule][0]
+            Cp_fit_nasa1=R*(coefficient_nasa1[0]*x**(-2)+coefficient_nasa1[1]*x**(-1)+coefficient_nasa1[2]+coefficient_nasa1[3]*x+coefficient_nasa1[4]*x**2+coefficient_nasa1[5]*x**3+coefficient_nasa1[6]*x**4)
+        x2=np.arange(1000, Tmax, 0.01)
+        coefficient_this2=fit_dictionary[molecule][1]
+        Cp_fit_this2=R*(coefficient_this2[0]*x2**(-2)+coefficient_this2[1]*x2**(-1)+coefficient_this2[2]+coefficient_this2[3]*x2+coefficient_this2[4]*x2**2+coefficient_this2[5]*x2**3+coefficient_this2[6]*x2**4)
+        if molecule in fit_nasa:
+            coefficient_nasa2=fit_nasa[molecule][1]
+            Cp_fit_nasa2=R*(coefficient_nasa2[0]*x**(-2)+coefficient_nasa2[1]*x**(-1)+coefficient_nasa2[2]+coefficient_nasa2[3]*x+coefficient_nasa2[4]*x**2+coefficient_nasa2[5]*x**3+coefficient_nasa2[6]*x**4)
+        x=np.hstack((x1,x2))
+        Cp_fit_this=np.hstack(( Cp_fit_this1, Cp_fit_this2))
+
+    plt.plot(x,Cp_fit_this,color="red",label="This work")    
+
+
+    if molecule in fit_nasa:
+        exist_nasa=True
+        if Tmax<=1000:
+            x_nasa=np.arange(298.15, Tmax, 0.01)
+            coefficient_nasa=fit_nasa[molecule][0]
+            Cp_fit_nasa=R*(coefficient_nasa[0]*x_nasa**(-2)+coefficient_nasa[1]*x_nasa**(-1)+coefficient_nasa[2]+coefficient_nasa[3]*x_nasa+coefficient_nasa[4]*x_nasa**2+coefficient_nasa[5]*x_nasa**3+coefficient_nasa[6]*x_nasa**4)
+        else:
+            x_nasa1=np.arange(298.15, 1000, 0.01)
+            coefficient_nasa1=fit_nasa[molecule][0]
+            Cp_fit_nasa1=R*(coefficient_nasa1[0]*x_nasa1**(-2)+coefficient_nasa1[1]*x_nasa1**(-1)+coefficient_nasa1[2]+coefficient_nasa1[3]*x_nasa1+coefficient_nasa1[4]*x_nasa1**2+coefficient_nasa1[5]*x_nasa1**3+coefficient_nasa1[6]*x_nasa1**4)
+            x_nasa2=np.arange(1000, Tmax, 0.01)
+            coefficient_nasa2=fit_nasa[molecule][1]
+            Cp_fit_nasa2=R*(coefficient_nasa2[0]*x_nasa2**(-2)+coefficient_nasa2[1]*x_nasa2**(-1)+coefficient_nasa2[2]+coefficient_nasa2[3]*x_nasa2+coefficient_nasa2[4]*x_nasa2**2+coefficient_nasa2[5]*x_nasa2**3+coefficient_nasa2[6]*x_nasa2**4)
+            x_nasa=np.hstack((x_nasa1,x_nasa2))
+            Cp_fit_nasa=np.hstack(( Cp_fit_nasa1, Cp_fit_nasa2))
+
+        plt.plot(x_nasa,Cp_fit_nasa,color="black",label="NASA Glenn")   
+
+    storepath="pictures"
+    storename=storepath+"/"+molecule
+
+
+
+    plt.legend()
+    plt.ylabel('$C_{p}$')
+    plt.xlabel('T(K)')
+    plt.title('Specific Heat Fit For '+molecule)
+    plt.savefig(storename)
+    plt.show()
 # %%
